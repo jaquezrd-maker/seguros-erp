0d90916a1099ff51c563511c7ca787e42351539a
Emmanuel Jaquez <jaquezrd@gmail.com>
2026-02-08 02:50:07 -0400

    Remove Vercel types from test

--- backend/api/test.ts
+++ backend/api/test.ts
@@ -1,6 +1,4 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default function handler(_req: VercelRequest, res: VercelResponse) {
+export default function handler(_req: any, res: any) {
   return res.json({
     status: 'ok',
     message: 'Backend is working!',
